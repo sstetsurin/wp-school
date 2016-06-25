c4b90b96b30579db60d541063496ca196ae162bb
kaname52 <kaname52@yandex.ru>
2016-06-25 22:15:28 +0300

    left sidebar menu js fix

--- wp-content/themes/davebrook/src/js/main.js
+++ wp-content/themes/davebrook/src/js/main.js
@@ -8,19 +8,21 @@ $( ".sitemap-button" ).click(function (e) {
     $( ".sitemap" ).hide();
   }
 });
+// $(document).ready(function () {
+// 	$('.sidebar-menu .page_item_has_children').each(function () {
+// 	if ($(this).not('.current_page_ancestor')) {
+// 		console.log('msg')
+// 		$(this).find('ul.children').collapse('hide');
+// 	} else $(this).find('ul.children').collapse('toggle');
+// });
+// })
 
-$('.sidebar-menu .page_item_has_children').each(function () {
-	if ($(this).not('.current_page_ancestor')) {
-		console.log('msg')
-		$(this).find('ul.children').collapse('hide');
-	} else $(this).find('ul.children').collapse('toggle');
-	
-	// console.log('msg')
-});
 //Collapse for sedebar submenu
-$('.sidebar-menu .page_item_has_children').click(function (e) {
-	e.preventDefault();
-	if ($(this).find('ul.children').hasClass('in')) {
-		$(this).find('ul.children').collapse('hide')
-	} else ($(this).find('ul.children').collapse('show'))
-})
+// $('.sidebar-menu .page_item_has_children').click(function (e) {
+// 	e.preventDefault();
+// 	if ($(this).find('ul.children').hasClass('in')) {
+// 		$(this).find('ul.children').collapse('hide')
+// 	} else ($(this).find('ul.children').collapse('show'))
+// })
+$('.sidebar-menu .page_item_has_children');
+console.log($(".page_item_has_children"))
